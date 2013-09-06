c69513e28079b0377641ff551844fb09a97fea73
Craig Martek <craigmartek@gmail.com>
2013-09-05 19:12:09 -0700

    Update frontpage

--- comingsoon/views.py
+++ comingsoon/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.template import RequestContext
+from groups.models import Group
 
 def index(request):
   return render(request, 'comingsoon/index.html',
+      dictionary={'group_count': Group.objects.count()},
       context_instance=RequestContext(request))
