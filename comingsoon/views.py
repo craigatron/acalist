b6296654ec8e5f8a9f56def2d68e44c145c9efda
Craig Martek <craigmartek@gmail.com>
2013-11-04 15:20:31 -0800

    About page

--- comingsoon/views.py
+++ comingsoon/views.py
@@ -6,3 +6,7 @@ def index(request):
   return render(request, 'comingsoon/index.html',
       dictionary={'group_count': Group.objects.count()},
       context_instance=RequestContext(request))
+
+def about(request):
+  return render(request, 'comingsoon/about.html',
+      context_instance=RequestContext(request))
